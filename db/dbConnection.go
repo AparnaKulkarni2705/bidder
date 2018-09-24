226503fc48cc36118cfeeee559f0cfcb2a5a4ce1
Aparna Kulkarni <aparnahkulkarni@gmail.com>
2018-09-25 01:46:10 +0530

    Added Insert method on Bidder model and added index on email for Bidder model
    - For search purposes, added index on email of bidder

--- db/dbConnection.go
+++ db/dbConnection.go
@@ -1,7 +1,7 @@
 package db
 
 import (
-    "gopkg.in/mgo.v2"
+    mgo "gopkg.in/mgo.v2"
     "log"
 )
 
@@ -15,4 +15,23 @@ func init(){
 	panic(err)
     }
     MgoSession = session
+    ensureIndexes()
+}
+
+func ensureIndexes(){
+    ensureIndexOnBidder()
+}
+
+func ensureIndexOnBidder(){
+     index := mgo.Index{
+	Key: []string{"email"},
+	Unique: true,
+	Background: true,
+    }
+
+    err := MgoSession.DB("di_bidder_db").C("bidder").EnsureIndex(index)
+    if err!=nil{
+	log.Fatal("Unique index does not exist on collection : bidder")
+	panic("Unique index does not exist on collection : bidder")
+    }
 }
